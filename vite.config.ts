c5ddfee1988a334c52064d350fc055c8016d32ef
harleen001 <harleen.johal31@gmail.com>
2026-09-17 21:13:12 +0530

    some

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
+    react(),
   ],
-  nitro: {
-    preset: 'vercel'
-  }
 })
